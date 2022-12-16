90231948e3f33e514d97e295912ff467f5bd26cc
HamidReza <delshad.swdn@gmail.com>
2022-12-16 01:03:44 +0100

    add contact info

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,6 +1,28 @@
-import NavBar from "../../components/NavBar/NavBarMain";
+import "./Contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <h1>Contact Us</h1>
+      <p>
+        We love hearing from you! Please contact us via the email address or
+        phone numbers below. For answer to frequently asked questions about your
+        subscription, please check out our FAQ page
+      </p>
+      <p>
+        <strong>Email address: </strong>help@yummy.com
+      </p>
+      <p>We will respond to your email within 72 hours.</p>
+      <p>
+        <strong>Toll-free number</strong>: 885-753-1037
+        <br />
+        <strong>International number:</strong> 332-239-2921
+      </p>
+      <p>
+        <strong>Hours of operation:</strong> <br />
+        Monday through Friday, 9am to 7pm E.T.
+      </p>
+    </div>
+  );
 };
 export default Contact;
